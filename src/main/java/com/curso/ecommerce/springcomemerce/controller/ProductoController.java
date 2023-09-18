93ef2952fbe7b3f3b9b1daeeb4a5f36d70e7b7d5
Juan Gadpen <juangadpen@hotmail.com>
2023-09-17 22:09:14 -0300

    Update productos

--- src/main/java/com/curso/ecommerce/springcomemerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/springcomemerce/controller/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -56,6 +57,23 @@ public class ProductoController {
 
     }
 
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id,Model model){
+        Producto producto;
+
+        Optional<Producto> optionalProducto=productoService.get(id);
+        producto=optionalProducto.get();
+
+        LOGGER.info("Producto buscado:{}",producto);
+        model.addAttribute("producto",producto);
+        return "productos/edit";
+    }
+
+@PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
 
 
 }
