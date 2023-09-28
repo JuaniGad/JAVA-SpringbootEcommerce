972a8f98f20029e0c6c74b2901964513a107905b
Juan Gadpen <juangadpen@hotmail.com>
2023-09-27 23:08:09 -0300

    Controllers and home/products templates

--- src/main/java/com/curso/ecommerce/springcomemerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/springcomemerce/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.curso.ecommerce.springcomemerce.controller;
 
 import com.curso.ecommerce.springcomemerce.service.ProductoService;
+import org.apache.coyote.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    //Tengo una variable de tipo log
+    private Logger log = LoggerFactory.getLogger(HomeController.class);
  @Autowired
     private ProductoService productoService;
 
@@ -21,4 +27,12 @@ public class HomeController {
         return "usuario/home";
     }
 
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id,Model model){
+        log.info("Id del producto enviado como parametro {}",id);
+
+        model.addAttribute("producto",productoService.get(id));
+
+        return "usuario/productohome";
+    }
 }
